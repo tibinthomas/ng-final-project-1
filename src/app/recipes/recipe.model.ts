9281c622d06d386bddd544eee163337b82de0374
Tibin Thomas <tibinthomas@outlook.com>
2017-06-24 12:41:34 +0530

    Recipe model done

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+}

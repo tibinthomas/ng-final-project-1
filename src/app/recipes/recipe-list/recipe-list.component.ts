47b43f0e5f2920ccfccaa0cbb2f9b5bcb7813bc2
Tibin Thomas <tibinthomas@outlook.com>
2017-06-24 12:48:55 +0530

    Populate recipe array with 3 dummy recipes in recipe-list.component.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Recipe name 1', 'Description of recipe 1', 'http://weknowyourdreams.com/image.php?pic=/images/food/food-07.jpg'),
+    new Recipe('Recipe name 2', 'Description of recipe 2', 'http://weknowyourdreams.com/image.php?pic=/images/food/food-07.jpg'),
+    new Recipe('Recipe name 3', 'Description of recipe 3', 'http://weknowyourdreams.com/image.php?pic=/images/food/food-07.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit() {

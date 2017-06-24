d9bc4a43f16ff1de82264b729b99dd5d157f64a7
Tibin Thomas <tibinthomas@outlook.com>
2017-06-24 23:14:42 +0530

    Add navigation using event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedComponent = 'shopping-list';
+
+  onEventReceive(event) {
+    this.selectedComponent = event;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
     selector: 'app-header',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent { }
+export class HeaderComponent {
+
+    @Output() selectedComponentEvent = new EventEmitter<string>();
+
+    onSelect(componentSelected: string) {
+        this.selectedComponentEvent.emit(componentSelected);
+    }
+
+}

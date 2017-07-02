746184200a1b66269d5b1371c792c4f1da6c93bb
Tibin Thomas <tibinthomas@outlook.com>
2017-07-02 15:30:15 +0530

    Routing cont..
    1) Passing dynamic parametes to links

--- src/app/recipes/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-item/recipe-item.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RecipeItemComponent implements OnInit {
 
  @Input() recipe: Recipe;
+ @Input() index: number;
 
   ngOnInit() {
   }

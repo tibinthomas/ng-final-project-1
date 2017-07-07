7e6829d36f2cea3d17a13eba6d916e944d8e232d
Tibin Thomas <tibinthomas@outlook.com>
2017-07-07 16:52:02 +0530

    Forms...Reactive...
    1) Adding more ingredients control

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -55,4 +55,13 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onAddNewIngredientInEditPage() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
+
 }

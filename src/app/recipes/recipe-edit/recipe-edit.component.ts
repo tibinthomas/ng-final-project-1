19c1101f4dcf3d4e27407b6cfece26ee9f8ff40b
Tibin Thomas <tibinthomas@outlook.com>
2017-07-07 17:17:52 +0530

    Forms....Reactive...
    1) Disabling save button for invalid form
    
    2) Some css work to show 2px solid red border if invalid after touch

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,7 @@
 import { RecipeService } from './../recipe.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -40,17 +40,17 @@ export class RecipeEditComponent implements OnInit {
       if (recipe['ingredients']) {
         for (const ingredient of recipe.ingredients) {
           recipeIngredients.push(new FormGroup( {
-            'name': new FormControl(ingredient.name),
-            'amount': new FormControl(ingredient.price)
+            'name': new FormControl(ingredient.name, Validators.required),
+            'amount': new FormControl(ingredient.price, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
           }));
         }
       }
     }
 
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription, Validators.required),
       'ingredients': recipeIngredients
     });
   }

a9d3a01e57bf36fb70586c7949a9c18617c1facc
Tibin Thomas <tibinthomas@outlook.com>
2017-07-10 10:40:22 +0530

    Auth...
    1) setting up firebase sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   selectedComponent = 'shopping-list';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBoUUnm3dwuqQJVcYNzWKglZiI14OB2dWI",
+      authDomain: "ng-final-project.firebaseapp.com"
+    })
+  }
 }
